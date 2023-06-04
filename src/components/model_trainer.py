afc5eecfc32d56624ace5c7a5abad98bc9caf568
BalaKarthikeya19 <82457691+BalaKarthikeya19@users.noreply.github.com>
2023-06-04 12:41:07 +0530

    model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -20,21 +20,22 @@ from src.logger import logging
 from src.utils import save_object,evaluate_models
 
 @dataclass
-class model_trainer_config:
-    trained_model_file=os.path.join("artifacts", "model.pkl")
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config =ModelTrainerConfig()
-    
-    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+        self.model_trainer_config=ModelTrainerConfig()
+
+
+    def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info("Split traing and tes input data")
-            X_train, y_train, X_test, y_test=(
-                train_array[:, :-1],
-                train_array[:, -1],
-                test_array[:, :-1],
-                test_array[:, -1]
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
             )
             models = {
                 "Random Forest": RandomForestRegressor(),
@@ -44,8 +45,7 @@ class ModelTrainer:
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
-            }    
-
+            }
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -86,10 +86,12 @@ class ModelTrainer:
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
                                              models=models,param=params)
+            
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
             ## To get best model name from dict
+
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
