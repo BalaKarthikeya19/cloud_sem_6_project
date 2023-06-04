def7cab93a031781616ce0e92ef00cc3bcf5414b
BalaKarthikeya19 <82457691+BalaKarthikeya19@users.noreply.github.com>
2023-06-04 22:50:15 +0530

    final files

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-
-hyphen_e_dot='-e .'
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
-    '''Returns a list of requirements'''
-
-    requirements = []
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
-        if hyphen_e_dot in requirements:
-            requirements.remove(hyphen_e_dot)
-    return requirements        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 
 
